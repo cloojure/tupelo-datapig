4f7e0af9b1a3bfb42c288dd81552151ab5970433
Alan Thompson <cloojure@gmail.com>
2016-05-16 21:59:46 -0700

    misc

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies   [ 
-    [cheshire                         "5.5.0"]
+    [cheshire                         "5.6.1"]
     [com.mchange/c3p0                 "0.9.5.2"]
-    [criterium                        "0.4.3"]
+    [criterium                        "0.4.4"]
     [java-jdbc/dsl                    "0.1.3"]
-    [org.apache.solr/solr-solrj       "5.4.0"]
+    [org.apache.solr/solr-solrj       "6.0.0"]
     [org.clojure/clojure              "1.8.0"]
-    [org.clojure/java.jdbc            "0.4.2"] 
-    [org.postgresql/postgresql        "9.4-1206-jdbc42"]
-    [prismatic/schema                 "1.0.4"]
-    [tupelo                           "0.1.58"]
+    [org.clojure/java.jdbc            "0.6.1"] 
+    [org.postgresql/postgresql        "9.4.1208"]
+    [prismatic/schema                 "1.1.1"]
+    [tupelo                           "0.1.71"]
   ]
   :main ^:skip-aot loader.core
   :target-path "target/%s"
